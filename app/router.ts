b87257b3a03efe5f7ec270f0901bf15cc237c631
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2019-01-18 18:01:46 +0800

    feat: update router

--- app/router.ts
+++ app/router.ts
@@ -4,6 +4,8 @@ export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
+  router.get('/docs', controller.home.docs);
+  router.get('/auth', controller.home.auth);
 
   router.post('/api/user/register', controller.api.users.register);
   router.post('/api/user/login', controller.api.users.login);
