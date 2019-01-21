756f6013d2c4733b7abbdf8107082924ca4f25ae
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2019-01-21 18:04:41 +0800

    feat: update view

--- app/web/src/pages/index.tsx
+++ app/web/src/pages/index.tsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import styles from './index.less';
 import { Button } from 'antd';
 
+// import Link from 'umi/link';
+// <Link to="/about">go to /about</Link>
 export default class Index extends Component {
   render() {
     return (
       <div className="container">
-        <Button type="primary">Press Me</Button>
+        <div className={styles.home}>
+          <Button type="primary">Press Me</Button>
+        </div>
       </div>
     );
   }
