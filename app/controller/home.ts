b97bde31335eba444f2d2f0c977f8e04be61f624
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2019-01-21 18:04:22 +0800

    feat: update router

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -2,26 +2,26 @@ import { Controller } from 'egg';
 
 export default class HomeController extends Controller {
   public async index() {
-    const { ctx } = this;    
+    const { ctx } = this;
     await ctx.render('react.nj', {
       title: 'Egg.js for Hasura',
-      message: 'Egg.js for Hasura.',      
-    });    
+      message: 'Egg.js for Hasura.',
+    });
   }
 
   public async docs() {
-    const { ctx } = this;    
+    const { ctx } = this;
     await ctx.render('docs.nj', {
       title: 'Egg.js for Hasura',
-      message: 'Egg.js for Hasura.',      
-    });    
+      message: 'Egg.js for Hasura.',
+    });
   }
-  
+
   public async auth() {
     const { ctx } = this;
     await ctx.render('auth.nj', {
       title: 'Egg.js for Hasura',
-      message: 'Egg.js for Hasura.',      
+      message: 'Egg.js for Hasura.',
     });
   }
 }
--- app/router.ts
+++ app/router.ts
@@ -3,10 +3,10 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
 
-  router.get('/', controller.home.index);
-  router.get('/docs', controller.home.docs);
-  router.get('/auth', controller.home.auth);
-
   router.post('/api/user/register', controller.api.users.register);
   router.post('/api/user/login', controller.api.users.login);
+  
+  router.get('/docs', controller.home.docs);
+  router.get('/auth', controller.home.auth);
+  router.get('*', controller.home.index);  
 };
